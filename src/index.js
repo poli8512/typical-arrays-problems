23cc0c8e180003ae7fbd697a95e6b5ee18976b76
poli8512 <poli8512@gmail.com>
2021-02-22 09:23:36 +0300

    'taskSolution'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(Array.isArray(array)){
+        console.log(JSON.stringify(array).length>3)
+        if(JSON.stringify(array).length>2){
+            return Math.min(...array)
+        }else{
+            return 0
+        }
+    }
+    else{return 0}
+
+
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(Array.isArray(array)){
+        console.log(JSON.stringify(array).length>3)
+        if(JSON.stringify(array).length>2){
+            return Math.max(...array)
+        }else{
+            return 0
+        }
+    }
+    else{return 0}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+
+    if(Array.isArray(array)){
+        console.log(JSON.stringify(array).length>3)
+        if(JSON.stringify(array).length>2){
+            return array.reduce((c,acc)=> acc+c)/array.length
+        }else{
+            return 0
+        }
+    }
+    else{return 0}
 }
